e1afcecaa39e7f6de28b577641c3d2296f54aa35
lisa <lisasoftgreat@gmail.com>
2020-07-25 19:02:31 +0300

    Add Departament Employee models

--- backend/monsieurd/models.py
+++ backend/monsieurd/models.py
@@ -18,4 +18,35 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Position(models.Model):
+    name = models.Charfield(max_length=200)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    fix_in_us = models.DecimalField(max_digits=10, decimal_places=2)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    date_of_employment = models.DateField()
+    contract_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    contract_number = models.CharField(max_length=200)
+    descrption = models.TextField()
+
+class ProjectTimesheet(models.Model):
+    hours_in_month = model.IntegerField()
+    
+
+
+
+
+    
+
 # Create your models here.
